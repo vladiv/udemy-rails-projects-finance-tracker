a5472bc2746c12ddec279243844fe785a584e52d
Vladislav Vodenicharski <vodenicharski.vladislav@gmail.com>
2017-11-01 10:15:33 +0200

    Add user and stock association and display, delete some tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  resources :user_stocks, except: [:show, :edit, :update]
+
   root 'welcome#index'
 
   get 'my_portfolio', to: 'users#my_portfolio'
